f5bc7ec85ab001c45cac3ef258f9daa5225b5c05
Shivcharan Thirunavukkarasu <132028666+TechShivvy@users.noreply.github.com>
2023-07-22 14:08:20 +0530

    socket change

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:8080";
+const SOCKET_URL = process.env.SERVER_URL || "http://localhost:8080";
 // const SOCKET_URL = "http://192.168.0.108:8080";
 const socket = io(SOCKET_URL);
 // const socket=io();
