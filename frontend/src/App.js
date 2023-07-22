bdbd00c516a3ba6254fe5f2ab8db8436479bd0cc
Shivcharan Thirunavukkarasu <132028666+TechShivvy@users.noreply.github.com>
2023-07-22 18:24:50 +0530

    test custom route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/join" element={<Join />} />
         <Route path="/chat" element={<ChatRoom />} />
       </Routes>
