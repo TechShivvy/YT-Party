fe0ed63a2242ee7ae2d717f003f7df8175eeade3
Shivcharan Thirunavukkarasu <132028666+TechShivvy@users.noreply.github.com>
2023-07-23 16:19:38 +0530

    readme

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -3,7 +3,6 @@ import io from "socket.io-client";
 
 const SOCKET_URL = "https://yt-party-server.onrender.com";
 // const SOCKET_URL = process.env.SERVER_URL || "http://localhost:8080";
-// const SOCKET_URL = "http://192.168.0.108:8080";
 const socket = io(SOCKET_URL);
 // const socket=io();
 export default socket;
